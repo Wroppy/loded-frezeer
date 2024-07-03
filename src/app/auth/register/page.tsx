6ee83c56cec214a0acd7fa5a08430df6c0cb5e5b
Weyman Wong <wxwong806@gmail.com>
2024-07-03 22:37:00 +1200

    Added sign in functionality and error checking in the register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -5,17 +5,41 @@ import AuthComponent from "../AuthComponent/AuthComponent";
 import EmailInput from "../EmailInput";
 import PasswordInput from "../PasswordInput";
 import NameInput from "../NameInput";
+import { postFetch } from "@/app/utils/postFetch";
+import { showErrorMessage } from "@/app/utils/showErrorMessage";
+import { RegisterResponse } from "@/app/types/RegisterResponse";
+import { signIn } from "next-auth/react";
 
 type Props = {};
 
 const RegisterPage = (props: Props) => {
-  const onSubmit = (event: FormEvent) => {
+  const onSubmit = async (event: FormEvent) => {
     event.preventDefault();
 
     // Gets the name, email and password from the form
     const name = (event.target as any).name.value as string;
     const email = (event.target as any).email.value as string;
     const password = (event.target as any).password.value as string;
+
+    // Registers the user using credentials
+    const result = await postFetch("/api/register", { name, email, password }) as RegisterResponse;
+
+    if (!result) {
+      showErrorMessage("An error occurred", "Please try again.");
+      return;
+    }
+    
+    if (result.error) {
+      showErrorMessage(result.error, "Please try again.");
+      return;
+    }
+
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
   };
 
   return (
---
+++ src/app/utils/postFetch.ts
@@ -0,0 +1,16 @@
+// A general purpose post fetch function
+
+export const postFetch = async (url: string, data: any) => {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
