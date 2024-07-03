a39cc8f53fdbcb9558aa254a99bf0caa33e3403f
Weyman Wong <wxwong806@gmail.com>
2024-07-03 21:29:39 +1200

    Changed sign in page path from sing-in to signin

--- src/app/api/auth/[...nextauth]/auth_options.ts
+++ src/app/api/auth/[...nextauth]/auth_options.ts
@@ -2,4 +2,8 @@ import { NextAuthOptions } from "next-auth";
 
 export const options: NextAuthOptions = {
   providers: [],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+  },
 };
