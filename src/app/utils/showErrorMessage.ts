892f29f580eb7d796a9d4cc3addba84784b9a2ae
Weyman Wong <wxwong806@gmail.com>
2024-07-03 22:37:20 +1200

    Removed console.log statement

--- src/app/utils/showErrorMessage.ts
+++ src/app/utils/showErrorMessage.ts
@@ -1,7 +1,6 @@
 import { notifications } from "@mantine/notifications";
 
 export const showErrorMessage = (title: string, message: string) => {
-  console.log("Hello");
   notifications.show({
     color: "red",
     title,
