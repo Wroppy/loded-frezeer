ed0f7e7d6ecfeb8ca3a1fd82f3dbd918500c75d2
Weyman Wong <wxwong806@gmail.com>
2024-07-04 22:06:39 +1200

    added linked to the nav bar drawer

--- src/app/components/navbar-drawer/NavBarDrawer.tsx
+++ src/app/components/navbar-drawer/NavBarDrawer.tsx
@@ -1,18 +1,49 @@
+"use client";
+
 import { useDisclosure } from "@mantine/hooks";
-import { Drawer } from "@mantine/core";
+import { Drawer, NavLink } from "@mantine/core";
+import {
+  IconBasket,
+  IconDashboard,
+  IconHome,
+  IconPigMoney,
+  IconWash,
+} from "@tabler/icons-react";
 
 import React from "react";
+import { usePathname } from "next/navigation";
 
 type Props = {
   opened: boolean;
   close: () => void;
 };
 
-const NavBarDrawer = ({opened, close}: Props) => {
-  return <Drawer opened={opened} onClose={close} title={"Loded Frezeer"}>
-    <div>Drawer content</div>
-    
-  </Drawer>
+const NavBarDrawer = ({ opened, close }: Props) => {
+  const links = [
+    { title: "Dashboard", href: "/", Icon: IconDashboard },
+    { title: "Shop", href: "/shoppinglist", Icon: IconBasket },
+    { title: "Expenses", href: "/expenses", Icon: IconPigMoney },
+    { title: "Chores", href: "/chores", Icon: IconWash },
+    { title: "Flatmates", href: "/flatmates", Icon: IconHome },
+  ];
+
+  const path = usePathname();
+
+  return (
+    <Drawer opened={opened} onClose={close} title={"Loded Frezeer"}>
+      {links.map(({ title, href, Icon }) => (
+        <NavLink
+          key={title}
+          leftSection={<Icon />}
+          onClick={close}
+          href={href}
+          label={title}
+          variant="light"
+          active={path === href}
+        />
+      ))}
+    </Drawer>
+  );
 };
 
 export default NavBarDrawer;
