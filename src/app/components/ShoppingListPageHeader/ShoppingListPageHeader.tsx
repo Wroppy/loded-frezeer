dd362972cf91acf57a351a53c8539190c05743f1
Weyman Wong <wxwong806@gmail.com>
2024-07-05 21:29:10 +1200

    added control to text input and number input

--- src/app/components/ShoppingListPageHeader/ShoppingListPageHeader.tsx
+++ src/app/components/ShoppingListPageHeader/ShoppingListPageHeader.tsx
@@ -1,21 +1,28 @@
 "use client";
 
 import { Button, NumberInput, TextInput, rem } from "@mantine/core";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./../ShoppingList/shopping-list.module.scss";
 import { IconDumpling, IconHash } from "@tabler/icons-react";
 
 type Props = {};
 
 const ShoppingListHeader = (props: Props) => {
+  const [itemName, setItemName] = useState("");
+  const [quantity, setQuantity] = useState<string | number>(1);
+
   return (
     <form className={styles.ShoppingListPageHeader}>
       <TextInput
         style={{ flexGrow: 1 }}
-        leftSection={<IconDumpling style={{ width: rem(16), height: rem(16) }} />}
+        leftSection={
+          <IconDumpling style={{ width: rem(16), height: rem(16) }} />
+        }
         placeholder="Add an item"
       />
       <NumberInput
+        value={quantity}
+        onChange={setQuantity}
         leftSection={<IconHash style={{ width: rem(16), height: rem(16) }} />}
         placeholder="Quantity"
         min={1}
