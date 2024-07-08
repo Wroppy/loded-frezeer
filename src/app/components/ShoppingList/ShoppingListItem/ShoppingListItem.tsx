1cc1faed8aa8b9d460344333b2f4b18b51621a9e
Weyman Wong <wxwong806@gmail.com>
2024-07-09 00:25:37 +1200

    Added strikethrough to shopping item text when the item has been purchased and disabled the checkbox

--- src/app/components/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
+++ src/app/components/ShoppingList/ShoppingListItem/ShoppingListItem.tsx
@@ -1,18 +1,23 @@
 import { ShoppingItem } from "@/app/types/ShoppingItem";
 import React, { ChangeEvent } from "react";
 import styles from "./shopping-list-item.module.scss";
-import { Card, Checkbox, Table } from "@mantine/core";
+import { Card, Checkbox, Table, Text } from "@mantine/core";
 
 type Props = {
   shoppingItem: ShoppingItem;
   selected: ShoppingItem | null;
   handleClick: (id: ShoppingItem) => void;
   checkedItems: string[];
-  setCheckedItems: (items: string[]) => void
+  setCheckedItems: (items: string[]) => void;
 };
 
-const ShoppingListItem = ({ shoppingItem, selected, handleClick, checkedItems, setCheckedItems }: Props) => {
-  
+const ShoppingListItem = ({
+  shoppingItem,
+  selected,
+  handleClick,
+  checkedItems,
+  setCheckedItems,
+}: Props) => {
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     // If the item is already checked, removes it from the checked items
     if (checkedItems.includes(shoppingItem.id)) {
@@ -23,7 +28,7 @@ const ShoppingListItem = ({ shoppingItem, selected, handleClick, checkedItems, s
     // Adds the item to the checked items
     setCheckedItems([...checkedItems, shoppingItem.id]);
   };
-  
+
   return (
     // Is selected if the selected prop matches the id of the shopping item
     <Card
@@ -40,9 +45,15 @@ const ShoppingListItem = ({ shoppingItem, selected, handleClick, checkedItems, s
           type="checkbox"
           checked={checkedItems.includes(shoppingItem.id)}
           onChange={onChange}
+          disabled={shoppingItem.boughtBy !== null}
         />
       </div>
-      <div className={styles.ShoppingListItemName}>{shoppingItem.itemName}</div>
+      <Text
+        td={shoppingItem.boughtBy !== null ? "line-through": "none"}
+        className={styles.ShoppingListItemName}
+      >
+        {shoppingItem.itemName}
+      </Text>
       <div>{shoppingItem.quantity}</div>
     </Card>
   );
