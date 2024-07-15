0bf381e4052568d58add1938f66ccd74f91f6cce
Weyman Wong <wxwong806@gmail.com>
2024-07-15 13:26:38 +1200

    added link navigation to the add chore page when

--- src/app/components/add-chore-box/AddChoreBox.tsx
+++ src/app/components/add-chore-box/AddChoreBox.tsx
@@ -1,16 +1,32 @@
+"use client";
+
 import React from "react";
 import ChoreBox from "../chore-box/ChoreBox";
 import styles from "./add-chore-box.module.scss";
 import { IconTablePlus } from "@tabler/icons-react";
-import { rem } from "@mantine/core";
+import { Flex, rem } from "@mantine/core";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 const AddChoreBox = (props: Props) => {
+  const router = useRouter();
+
   return (
     <ChoreBox className={styles.AddChoreBox} dotted>
-      <IconTablePlus style={{ width: rem(80), height: rem(80) }} stroke={1.5} />
-      <div>Add a new chore</div>
+      <Flex
+        onClick={() => router.push("/chores/add-chore")}
+        align={"center"}
+        justify={"center"}
+        direction={"column"}
+        style={{ width: "100%", height: "100%", cursor: "pointer" }}
+      >
+        <IconTablePlus
+          style={{ width: rem(80), height: rem(80) }}
+          stroke={1.5}
+        />
+        <div>Add a new chore</div>
+      </Flex>
     </ChoreBox>
   );
 };
