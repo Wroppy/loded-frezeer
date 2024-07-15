0f9d2d1d312d418869684b37fc58c3d08bfb77cc
Weyman Wong <wxwong806@gmail.com>
2024-07-15 13:21:11 +1200

    Added fetch request to the server to get the chores and displays them on the webpage given the email address of the user

--- src/app/chores/ChoresViewer.tsx
+++ src/app/chores/ChoresViewer.tsx
@@ -7,50 +7,19 @@ import ChoreCycles from "../Enums/ChoreCycles";
 import ChoreView from "../components/chore-view/ChoreView";
 import AddChoreBox from "../components/add-chore-box/AddChoreBox";
 import { useDisclosure } from "@mantine/hooks";
+import ClientUser from "../types/ClientUser";
 
-const CHORES: ClientChore[] = [
-  {
-    name: "Clean the house",
-    description: "Vacuum and mop the floors",
-    expectedCycle: ChoreCycles.Weekly,
-    lastCompleted: new Date(2021, 8, 1),
-    expectedUser: "John Doe",
-    nextExpectedUser: "Jane Doe",
-    id: "1",
-    previousUser: "Jack Doe",
-    order: [
-      { name: "Jane Joe", email: "jane@email.com" },
-      { name: "Jake Doe", email: "jake@email.com" },
-      { name: "John Doe", email: "john@email.com" },
-    ],
-  },
-  {
-    name: "Trash",
-    description: "Take out the trash",
-    expectedCycle: ChoreCycles.Daily,
-    lastCompleted: new Date(2021, 8, 1),
-    expectedUser: "John Doe",
-    nextExpectedUser: "Jane Doe",
-    id: "2",
-    order: [
-      { name: "John Doe", email: "john@email.com" },
-      { name: "Jane Joe", email: "jane@email.com" },
-      { name: "Jake Doe", email: "jake@email.com" },
-    ],
-  },
-];
+type Props = {
+  email: string;
+  chores: ClientChore[];
+  users: ClientUser[];
+};
 
-type Props = {};
-
-const ChoresViewer = (props: Props) => {
-  const [chores, setChores] = useState(CHORES);
+const ChoresViewer = ({ chores: c, email, users }: Props) => {
+  const [chores, setChores] = useState(c);
   const [toEditChore, setToEditChore] = useState<ClientChore | null>(null);
   const [opened, { open, close }] = useDisclosure(false);
-  const users = [
-    { name: "John Doe", email: "john@email.com" },
-    { name: "Jane Joe", email: "jane@email.com" },
-    { name: "Jake Doe", email: "jake@email.com" },
-  ];
+
   // Adds a new chore to the state
   const addCore = (chore: ClientChore) => {};
 
--- src/app/chores/page.tsx
+++ src/app/chores/page.tsx
@@ -1,13 +1,44 @@
 import React from "react";
 import styles from "./chore-page.module.scss";
 import ChoresViewer from "./ChoresViewer";
+import { getServerSession } from "next-auth";
+import ClientUser from "../types/ClientUser";
+import { postFetch } from "../utils/postFetch";
+import ClientChore from "../types/ClientChore";
 
 type Props = {};
 
-const page = (props: Props) => {
+const page = async (props: Props) => {
+  const session = await getServerSession();
+
+  if (!session) {
+    return null;
+  }
+
+  const email = session.user!.email!;
+
+  // Gets the users in the flat
+  const { users, error } = (await postFetch("/api/get-users", {
+    email,
+  })) as { users: ClientUser[]; error: string };
+
+  if (error) {
+    return null;
+  }
+
+  // Gets the chores
+  const { chores, error: e } = (await postFetch("/api/chores/get-chores", {
+    email,
+  })) as { chores: ClientChore[]; error: string };
+
+  if (e) {
+    return null;
+  }
+
+
   return (
     <div className={styles.ChorePage}>
-      <ChoresViewer />
+      <ChoresViewer email={email} chores={chores} users={users}/>
     </div>
   );
 };
