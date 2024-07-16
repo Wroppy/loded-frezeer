2200aaf18e23fbaaa21ff7c3843f6d17a94bbe08
Weyman Wong <wxwong806@gmail.com>
2024-07-16 17:29:08 +1200

    Added padded to the page and change visited link styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default async function NotFound() {
     <Flex
       justify={"center"}
       align={"center"}
-      style={{ width: "100%", height: "100%" }}
+      style={{ width: "100%", height: "100%", padding: "32px" }}
     >
       <Flex gap="lg">
         <Flex direction={"column"} align={"center"}>
@@ -14,7 +14,7 @@ export default async function NotFound() {
         </Flex>
         <Divider orientation={"vertical"} />
         <Flex direction={"column"}>
-          <div>Unfortunately the page you request was not found</div>
+          <div>Unfortunately the page you requested was not found</div>
           <div>
             Go back to <Link href="/">home</Link>
           </div>
