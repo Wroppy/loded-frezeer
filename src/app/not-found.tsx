6839ead6a4c13838804b5edd8fd67924dcdebfca
Weyman Wong <wxwong806@gmail.com>
2024-07-16 16:47:34 +1200

    Added a page not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { Divider, Flex } from "@mantine/core";
+import Link from "next/link";
+
+export default async function NotFound() {
+  return (
+    <Flex
+      justify={"center"}
+      align={"center"}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <Flex gap="lg">
+        <Flex direction={"column"} align={"center"}>
+          <h1>404</h1>
+        </Flex>
+        <Divider orientation={"vertical"} />
+        <Flex direction={"column"}>
+          <div>Unfortunately the page you request was not found</div>
+          <div>
+            Go back to <Link href="/">home</Link>
+          </div>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+}
