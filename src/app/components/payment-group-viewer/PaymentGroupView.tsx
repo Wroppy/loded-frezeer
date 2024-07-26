f35dd952266dda6221e3845a539b0a138204080f
Weyman Wong <wxwong806@gmail.com>
2024-07-26 14:45:48 +1200

    Added footer to the payment group card view

--- src/app/components/payment-group-viewer/PaymentGroupView.tsx
+++ src/app/components/payment-group-viewer/PaymentGroupView.tsx
@@ -1,25 +1,37 @@
+"use client";
+
 import PaymentGroup from "@/app/types/PaymentGroup";
-import { Card } from "@mantine/core";
-import React from "react";
+import { ActionIcon, Card, Flex } from "@mantine/core";
+import React, { useState } from "react";
 import styles from "./payment-group-view.module.scss";
+import ConfirmButton from "../confirm-button/ConfirmButton";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
 
 type Props = {
   paymentGroup: PaymentGroup;
 };
 
 const PaymentGroupView = ({ paymentGroup }: Props) => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <Card className={styles.PaymentGroupView}>
       <h2>{paymentGroup.name}</h2>
       <div>
-        <h4>
-          Members:
-        </h4>
+        <h4>Members:</h4>
         <div className={styles.UsersListView}>
           {paymentGroup.users.map((user, index) => (
             <div key={index}>{user.name}</div>
           ))}
         </div>
+        <Flex justify={"flex-end"} gap="sm">
+          <ConfirmButton text="Are you sure?" loading={loading} color="green">
+            <IconEdit style={{ width: "70%", height: "70%" }} stroke={1.5} />
+          </ConfirmButton>
+          <ConfirmButton text="Are you sure?" loading={loading}>
+            <IconTrash style={{ width: "70%", height: "70%" }} stroke={1.5} />
+          </ConfirmButton>
+        </Flex>
       </div>
     </Card>
   );
