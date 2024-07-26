eaebc48b4705d2ecdb709e45e485aeb030006c09
Weyman Wong <wxwong806@gmail.com>
2024-07-26 21:20:01 +1200

    Added payment group icon to the collpased sidebar

--- src/app/components/expenses-sidebar/CollapsedSidebar.tsx
+++ src/app/components/expenses-sidebar/CollapsedSidebar.tsx
@@ -1,4 +1,4 @@
-import { IconUser } from "@tabler/icons-react";
+import { IconCashBanknote, IconUser } from "@tabler/icons-react";
 import React from "react";
 import styles from "./expenses-siderbar.module.scss";
 
@@ -7,6 +7,7 @@ type Props = { visible: boolean };
 const CollapsedSidebar = ({ visible }: Props) => {
   return (
     <div className={styles.ExpensesSidebarHeader} style={{ display: visible ? "block" : "none" }}>
+      <IconCashBanknote className={styles.ExpensesSidebarUserIcon} />
       <IconUser className={styles.ExpensesSidebarUserIcon} />
     </div>
   );
