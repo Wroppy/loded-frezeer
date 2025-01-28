466b4fabddc36476fc7c8fdd1e84bb1e2dd352e0
Weyman Wong <wxwong806@gmail.com>
2025-01-28 23:00:44 +1300

    reformatted for code style

--- src/app/components/expenses-sidebar/AddExpenseButton.tsx
+++ src/app/components/expenses-sidebar/AddExpenseButton.tsx
@@ -1,14 +1,10 @@
-import { NavLink } from "@mantine/core";
 import React from "react";
 import SidebarNavLink from "./SidebarNavLink";
 
 type Props = {};
 
 const AddExpenseButton = (props: Props) => {
-  return <SidebarNavLink label="Add Expense" href="/expenses/add-expense"/>
-      
-    ;
-
+  return <SidebarNavLink label="Add Expense" href="/expenses/add-expense" />;
 };
 
 export default AddExpenseButton;
