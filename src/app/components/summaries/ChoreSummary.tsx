582141aefa4fc5d531a1bd5654b122f821f7f22e
Weyman Wong <wxwong806@gmail.com>
2025-02-06 23:05:45 +1300

    Added initial chore summary template

--- src/app/components/summaries/ChoreSummary.tsx
+++ src/app/components/summaries/ChoreSummary.tsx
@@ -1,9 +1,32 @@
+import ClientChore from "@/app/types/ClientChore";
 import React from "react";
+import styles from "./summaries.module.scss";
+import { Card } from "@mantine/core";
 
-type Props = {};
+type Props = {
+  chores: ClientChore[];
+};
 
-const ChoreSummary = (props: Props) => {
-  return <div>ChoreSummary</div>;
+const Summary = ({ chore }: { chore: ClientChore }) => {
+  return (
+    <Card className={styles.Summary}>
+      <div>{chore.name}</div>
+      <div>{chore.description}</div> 
+    </Card>
+  );
+};
+
+const ChoreSummary = ({ chores }: Props) => {
+  return (
+    <div className={styles.ChoresSummary}>
+      <div>Chores Summary</div>
+      <div className={styles.ChoresSummaryContent}>
+        {chores.map((chore) => (
+          <Summary key={chore.id} chore={chore} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ChoreSummary;
