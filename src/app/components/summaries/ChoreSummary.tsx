13e14b7712925e54b6170f00b164ae5bb16f4cdc
Weyman Wong <wxwong806@gmail.com>
2025-02-06 23:47:11 +1300

    Added chore summary name, description and timeline onto the component

--- src/app/components/summaries/ChoreSummary.tsx
+++ src/app/components/summaries/ChoreSummary.tsx
@@ -1,17 +1,26 @@
+"use client";
+
 import ClientChore from "@/app/types/ClientChore";
 import React from "react";
 import styles from "./summaries.module.scss";
 import { Card } from "@mantine/core";
+import ChoreView from "../chore-view/ChoreView";
+import ChoreTimeLine from "../chore-view/ChoreTimeLine";
 
 type Props = {
   chores: ClientChore[];
 };
 
 const Summary = ({ chore }: { chore: ClientChore }) => {
+  console.log(chore)
+  chore.lastCompleted = new Date(chore.lastCompleted);
   return (
-    <Card className={styles.Summary}>
-      <div>{chore.name}</div>
-      <div>{chore.description}</div> 
+    <Card>
+      <h2 className={styles.choreHeading}>{chore.name}</h2>
+      <div className={styles.choreDescription}>{chore.description}</div>
+      <div className={styles.choreTimeline}>
+        <ChoreTimeLine chore={chore} />
+      </div>
     </Card>
   );
 };
