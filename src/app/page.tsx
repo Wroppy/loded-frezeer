46511d1f19ad0789ff694f54dcef049c6be27e3e
Weyman Wong <wxwong806@gmail.com>
2025-02-06 23:37:09 +1300

    Added client fetch call to get dashboard data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,30 @@ import styles from "./page.module.scss";
 import ChoreSummary from "./components/summaries/ChoreSummary";
 import ShoppingListSummary from "./components/summaries/ShoppingListSummary";
 import ExpenseSummary from "./components/summaries/ExpenseSummary";
+import { getServerSession } from "next-auth";
+import { postFetch } from "./utils/postFetch";
 
-export default function Home() {
+import { redirect } from "next/navigation";
+import { GetDashboardResponse } from "./types/DashboardRoute";
+export default async function Home() {
+  const session = await getServerSession();
+  const email = session?.user?.email;
+
+  if (!email) {
+    redirect("/login");
+    return null;
+  }
+
+  const response = await postFetch("/api/dashboard/get", { email }) as GetDashboardResponse;
+  if (!response.success) {
+    return <div>Failed to load dashboard data</div>;
+  }
+
+  const {chores} = response;
+
   return (
     <main className={styles.main}>
-      <ChoreSummary />
+      <ChoreSummary chores={chores}/>
       <ShoppingListSummary />
       <ExpenseSummary />
     </main>
--- src/app/types/DashboardRoute.ts
+++ src/app/types/DashboardRoute.ts
@@ -1,6 +1,19 @@
+import ClientChore from "./ClientChore";
+import { ShoppingItem } from "./ShoppingItem";
+import UserExpenseSummary from "./UserExpenseSummary";
+
 type GetDashboardBody = {
   email: string;
 };
 
-type GetDashboardResponse = {};
+type GetDashboardResponse = {
+  success: boolean;
+  user: {
+    name: string;
+  };
+  chores: ClientChore[];
+  userShoppingList: ShoppingItem[];
+  expenses: UserExpenseSummary[];
+};
+
 export { type GetDashboardBody, type GetDashboardResponse };
